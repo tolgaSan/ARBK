1cdbdcef7256bad520b7fbc6054ddf328ca6db42
Tolga San <58885334+tolgaSan@users.noreply.github.com>
2022-11-06 19:23:22 +0100

    Add files via upload

--- Tolga_Sanli_3236111_ARBKVS_2/Tolga_Sanli_3236111_ARBKVS_2/main.c
+++ Tolga_Sanli_3236111_ARBKVS_2/Tolga_Sanli_3236111_ARBKVS_2/main.c
@@ -4,38 +4,80 @@
  * Created: 28.10.2022 09:27:35
  * Author : TolgaSanli
  */ 
-#define F_CPU 16000000UL //CPU hat eine Taktfrequenz von 16.000.000 Hertz
 #include <avr/io.h>
+#define F_CPU 16000000UL //CPU hat eine Taktfrequenz von 16.000.000 Hertz
 #include <util/delay.h>
 
+#define led0 PINB0
+#define led9 PINB1
+#define sw0 PIND2
+#define sw1 PIND3
+int blinkstatus = 1;
+int aktuellerLED = PINB0;
+int zustandDauerlicht = 1;
 
 int main(void)
 {
-	//Pin Eingänge und Ausgänge
-	//Alle Ports als Eingang, da alles auf 1
-	DDRD = 0x03;
-	//PORTD = ausgang definieren. Bzw. setzen
-	PORTD = 0b00000000;
-    //unendlicher loop
-    while (1) 
-    {
+	
+	DDRB = 0xff;
+	PORTB = 0x00;
+	
+	DDRD = 0x00;
+	PORTD = 0xff;
+	
+	
+	
+	while (1)
+	{
+		blinkmodus();
 		// &= binary and -> löschen die bits(?)
 		// |= binary or -> Wir setzen den Bit
 		// ^ exclusive or -> bit toggeln
 		// >> rotate right
 		// << rotate left
 		// ~ Bit invertierten
-		PORTD |= (1<<0); 
-	
-		//PORTD &= (1<<1);
-		_delay_ms(1000);
- 		PORTD &= (1<<1); // !0b00000001 AND 0b11111110 << eins setzen -> 0b00000001 ????????
-		_delay_ms(1000);
-		
-// 		PORTD |= (1<<1);
-// 		//_delay_ms(1000);
-// 		PORTD &= (1<<2); // !0b00000001 AND 0b11111110 << eins setzen -> 0b00000001 ????????
-// 		//_delay_ms(1000);
-    }
+		if(blinkstatus == 1)
+		{
+			PORTB &= ~(1<< (!aktuellerLED)); // 0000 0001 & 1111 1110
+			PORTB ^= (1 << aktuellerLED);	 // 0000 0001 xor 1111 1110 = 1111 1111
+		}
+		else
+		{
+			PORTB &= ~(1<< (!aktuellerLED));
+			PORTB |= (1 << aktuellerLED);
+		}
+		_delay_ms(250);
+	}
 }
 
+void blinkmodus(){
+	if(!(PIND & (1<<sw0)))
+	{
+		if(aktuellerLED == led0){
+			if(blinkstatus == 1)
+			{
+				blinkstatus = 0;
+			}
+			else
+			{
+				blinkstatus = 1;
+			}
+		}else{
+			aktuellerLED = led0;
+		}	
+	}
+	if(!(PIND & (1<<sw1)))
+	{
+		if(aktuellerLED == led9){
+			if(blinkstatus == 1){
+				blinkstatus = 0;
+			}
+			else
+			{
+				blinkstatus = 1;
+			}
+		}else{
+			aktuellerLED = led9;
+		}	
+	}
+}
